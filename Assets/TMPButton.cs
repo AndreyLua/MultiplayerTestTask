9598fac3fe99bcf2f59aed4db1c3e349d3eff9bd
AndreyLua <panzar.egorov1@yandex.ru>
2023-08-20 17:57:04 +0300

    added text mesh pro

---
+++ Assets/TMPButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TMPButton : MonoBehaviour
+{
+    private Button _screenButton;
+    private TextMeshProUGUI _text;
+
+    public string Text => _text.text;
+
+    public event Action Clicked;
+
+    private void Awake()
+    {
+        _text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        _screenButton = gameObject.GetComponent<Button>();
+        _screenButton.onClick.AddListener(OnClick);
+    }
+
+    protected virtual void OnClick()
+    {
+        Clicked?.Invoke();
+    }
+}
---
+++ Assets/TMPInputField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class TMPInputField : MonoBehaviour
+{
+    private TMP_InputField _inputField;
+
+    public event Action<string> InputStringChanged;
+
+    private void Awake()
+    {
+        _inputField = gameObject.GetComponent<TMP_InputField>();
+        _inputField.onValueChanged.AddListener(OnValueChanged);
+    }
+
+    private void OnValueChanged(string text)
+    {
+        InputStringChanged?.Invoke(text);
+    }
+}
